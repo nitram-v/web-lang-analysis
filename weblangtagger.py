f16920ee68f0f0d1c9a4bc27644dcea0a4607e41
nitram-v <martin.vainikko@gmail.com>
2022-06-07 13:08:34 +0300

    Cleaning up the code: removing commented out code blocks, adding comments

--- weblangtagger.py
+++ weblangtagger.py
@@ -122,9 +122,6 @@ class WebLangTagger(Tagger):
         
         self.clauses_layer = clauses_layer
         
-        #output_attributes=('word_count',)
-        #output_attributes=()
-        
         self.use_unknown_words = use_unknown_words
         self.use_emoticons = use_emoticons
         self.use_letter_reps = use_letter_reps
@@ -138,8 +135,8 @@ class WebLangTagger(Tagger):
         self.use_foreign_z_letters = use_foreign_z_letters
         self.use_emojis = use_emojis
         
-        self.input_layers = [words_layer, compound_tokens_layer] #clauses_layer
-        self.output_layer = output_layer #paragraphs_layer
+        self.input_layers = [words_layer, compound_tokens_layer]
+        self.output_layer = output_layer
         
         self.output_attributes = ['type']
         
@@ -153,29 +150,11 @@ class WebLangTagger(Tagger):
                 (i["pattern_type"]=="incorrect_spaces" and use_incorrect_spaces is True) or
                 (i["pattern_type"]=="foreign_z_letters" and use_foreign_z_letters is True))]
         
-        # only attribute flags that are True are added to output_attributes
-        #if use_emoticons is True:
-        #    output_attributes=output_attributes + ("emoticons",)
-        #if use_missing_commas is True:
-        #    output_attributes=output_attributes + ("missing_commas",)
-        #if use_unknown_words is True:
-        #    output_attributes=output_attributes + ("unknown_words",)
-        #if use_emojis is True:
-        #    output_attributes=output_attributes + ("emojis",)
-            
-        #if len(filtered_vocabulary) != 0: # attributes from vocabulary for regextagger
-        #    for i in filtered_vocabulary:
-        #        if not i["pattern_type"] in output_attributes:
-        #            output_attributes=output_attributes + (i["pattern_type"],)
-        #else:
-        #    output_attributes          
-        #self.output_attributes=output_attributes
-        
         self.regex_tagger = RegexTagger(vocabulary=filtered_vocabulary,
                                         output_layer='web_language',output_attributes=['pattern_type'],
                                         conflict_resolving_strategy='ALL')
         
-        # only used when these attribute flags are True
+        # Only used when these attribute flags are True
         if use_missing_commas is True:
             self.input_layers.append(clauses_layer)
             self.conf_param.append('clause_segmenter')
@@ -188,20 +167,15 @@ class WebLangTagger(Tagger):
             self.vabamorf_tagger = VabamorfTagger(guess=False,propername=False,disambiguate=False,phonetic=False,output_layer='morph_unknown_words')
         else:
             pass
-        
-    #def _make_layer_template(self):
-    #    return Layer(name=self.output_layer, text_object=None)
             
-    def _make_layer(self, text, layers, status): # status=None
+    def _make_layer(self, text, layers, status):
         # Create new layer based on the configuration
-        layer = Layer(name=self.output_layer, attributes=self.output_attributes, text_object=text, ambiguous=True) #attributes=self.output_attributes
-        
-        #layer = self._make_layer_template()
-        #layer.text_object = text
+        layer = Layer(name=self.output_layer, attributes=self.output_attributes, text_object=text, ambiguous=True) 
         
         words=layers['words']
         compound_tokens=layers['compound_tokens']
         
+        # Attribute counts are added to meta data
         layer.meta['punct_reps'] = 0
         layer.meta['letter_reps'] = 0
         layer.meta['no_spaces'] = 0
@@ -216,32 +190,38 @@ class WebLangTagger(Tagger):
         layer.meta['emojis'] = 0
         layer.meta['word_count'] = 0
         
+        # Tagging most of the attributes with regex tagger and saving the spans to a layer
         web_language_layer = self.regex_tagger.make_layer(text=text,layers=layers)
         
-        # only needed when these attribute flags are True
+        # Only needed when these attribute flags are True
+        # In this version, detecting missing commas and unknown words is not implemented
         if self.use_missing_commas == True:
             clauses=layers['clauses']
             ignore_missing_commas_clauses = self.clause_segmenter.make_layer(text=text,layers=layers)
         if self.use_unknown_words == True:
             morph_unknown_words = self.vabamorf_tagger.make_layer(text=text,status=status)
         
-        text_score=0 # a total number of all the attributes in a text
-        text_word_count=0 # a total number of words in a whole text
+        # A total number of all the attributes in a text
+        text_score=0
+        # A total number of words in a whole text
+        text_word_count=0
         
-        # checks if certain words belong to the same paragraph
+        # Checks if certain words belong to the same paragraph
+        # Text is not split into paragraphs, the whole text is tagged
+        # Since the whole text is tagged, this function always returns 'True'
         def if_in_paragraph(paragraph_i, new_i): 
             #if (paragraph_i.start == new_i.start or paragraph_i.start < new_i.start) and (paragraph_i.end == new_i.end or paragraph_i.end > new_i.end):
             return True
             
-        # checks for clauses that shouldn't be counted as attributes 
-        # clauses that end eg with a word "ainult" if the following clause starts eg with "et"
+        # Checks for clauses that shouldn't be counted as attributes 
+        # Clauses that end eg with a word "ainult" if the following clause starts eg with "et"
         def counter_comma(counter, word, new_list, clause, start_index):
             if counter!=0:
                 if clause.text[0].lower() in [word]:
                     new_list.append(start_index)
                     return new_list
         
-        # checks if a word consists of letters/numbers
+        # Checks if a word consists of letters/numbers
         def validateString(s):
             flag = False
             for i in s:
@@ -249,7 +229,7 @@ class WebLangTagger(Tagger):
                     flag = True
             return flag
         
-        # finds emojis from a string
+        # Finds emojis from a string
         # https://stackoverflow.com/questions/43146528/how-to-extract-all-the-emojis-from-text
         def find_emojis(text):
 
@@ -259,8 +239,8 @@ class WebLangTagger(Tagger):
                 if any(char in emoji.UNICODE_EMOJI['en'] for char in word):
                     emoji_list.append(word)
     
-            # Emojid on tekstist leitud, aga oleks vaja ka leida emojide asukohad tekstis:
-            # regexi abil saab leida igale emojile asukohad tekstis
+            # Emojis are found from the text, but the spans (coordinates in text) are not
+            # The spans are found with regex
             emoji_pos = []
             for word in set(emoji_list):
                 emoji_pos.extend([emoji for emoji in re.finditer(word, text)])
@@ -269,54 +249,52 @@ class WebLangTagger(Tagger):
 
             return emoji_pos + flags
         
-        # tsükli asemel otse säutsu Text objekt
+        # This is the string object of the text
         text_str = text.text
-        word_list=[] # a total number of words in text
-        attr_dict=defaultdict(int) # attributes counted separately in a text
+        # Total number of words in text
+        word_list=[]
+        # Attributes counted separately in a text
+        attr_dict=defaultdict(int)
             
-        for i in self.output_attributes: # by default attribute count is set to 0
+        # By default attribute count is set to 0
+        for i in self.output_attributes:
             if i != "emoticons" or i != "missing_commas" or i != "unknown_words" or i != "emojis":
                 attr_dict[i]=0
-                
+        
+        # Checking for conflicts between compound tokens layer and web language layer spans
+        # For example, preventing foreign letters being detected from a username mention
         for i in web_language_layer:
             if if_in_paragraph(text_str, i) == True:
                 check=0
                 attr=i.pattern_type
                 for cp in compound_tokens:
-                    # such compound tokens are not counted
-                    # muuta ilusamaks
-                    #print(cp.type)
+                    # Such compound tokens are not counted
                     if "www_address" in cp.type or "email" in cp.type or "non_ending_abbreviation" in cp.type or "username_mention" in cp.type or "hashtag" in cp.type or "emoticon" in cp.type:
                         for item in cp:
-                            #print("conflict", item)
                             if conflict(i,item):
                                 check+=1
                 if check==0:
-                    #attr_dict[attr] += 1
                     layer.meta[attr] += 1
-                    # kuidas lisada kihile märgendus?
                     layer.add_annotation( (i.start, i.end), **{'type':attr})
-                        
-        if self.use_emoticons == True: # attribute "emoticons"
+        
+        # Attribute "emoticons"
+        if self.use_emoticons == True:
             attr_dict["emoticons"]=0
             for cp in compound_tokens:
-                #print(type(cp))
-                #print(cp.start)
                 if "emoticon" in cp.type:
                     if if_in_paragraph(text_str, cp) == True:
-                        #attr_dict["emoticons"] += 1
                         layer.meta["emoticons"] += 1
                         layer.add_annotation( (cp.start, cp.end), **{'type':"emoticons"})
-                            
-        if self.use_emojis == True: # attribute "emojis"
+        
+        # Attribute "emojis"
+        if self.use_emojis == True:
             attr_dict["emojis"]=0
             emojis = find_emojis(text_str)
-            #attr_dict["emojis"] += len(emojis)
             layer.meta["emojis"] += len(emojis)
             [layer.add_annotation( (emoji.start(), emoji.end()), **{'type':"emoji"}) for emoji in emojis]
                     
-        ## TODO: lisada kihti
-        # finds clauses with a missing comma
+        ## NOT IMPLEMENTED
+        # Finds clauses with a missing comma
         if self.use_missing_commas == True:
             attr_dict["missing_commas"]=0
             not_suitable=[]
@@ -335,23 +313,26 @@ class WebLangTagger(Tagger):
                         if cl.start==w.start:
                             counter_comma(counter,"et",not_suitable,cl,w.start)
                             counter_comma(counter2,"kui",not_suitable,cl,w.start)
-                            
-                    if w.start not in [c.start for c in clauses if c]: # indexes of the beginnings of clauses
-                        if w.start in [c.start for c in ignore_missing_commas_clauses if c]: # indexes of the beginnings of clauses (also detects clauses that have a missing comma)
+                    
+                    # Indexes of the beginnings of clauses
+                    if w.start not in [c.start for c in clauses if c]:
+                        # Indexes of the beginnings of clauses (also detects clauses that have a missing comma)
+                        if w.start in [c.start for c in ignore_missing_commas_clauses if c]:
                             for cl in ignore_missing_commas_clauses:
-                                if w.start==cl.start: # if the start index of a clause is only in ignore_missing_commas_clauses, it means there is a missing comma
+                                # If the start index of a clause is only in ignore_missing_commas_clauses, it means there is a missing comma
+                                if w.start==cl.start:
                                     if w.start not in not_suitable: # 
                                         if w.text != "palun":
                                             attr_dict["missing_commas"] += 1
                     
 
-        # the number of words in a paragraph  
+        # The number of words in a paragraph (text in this case)  
         for w in words:
             if if_in_paragraph(text_str, w) == True:
                 word_list.append(w)
             
-        ## TODO: lisada kihti
-        # finds unknown words in a paragraph
+        ## NOT IMPLEMENTED
+        # Finds unknown words in a paragraph
         if self.use_unknown_words == True:
             attr_dict["unknown_words"]=0
             for morph in morph_unknown_words:
@@ -362,7 +343,8 @@ class WebLangTagger(Tagger):
                             count=0
                             for t,t2 in zip(compound_tokens.normalized, compound_tokens.type):
                                 if morph[0].text!=t and "emoticon" not in t2 and "name_with_initial" not in t2 and not match2:
-                                    count+=1 # such compound tokens are not counted as unknown words             
+                                    # Such compound tokens are not counted as unknown words  
+                                    count+=1           
                             if count !=0:
                                 attr_dict["unknown_words"] += 1
                             if morph[0].start not in text.compound_tokens["start"] and not match2:
